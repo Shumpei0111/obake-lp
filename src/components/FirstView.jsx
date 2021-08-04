c446acd1b506e127c4663c5ce1feb01b3c0c72b2
Shumepi <shumpei.o.7g@gmail.com>
2021-08-04 15:41:18 +0900

    Update TOPのロゴをふわっと現れて揺れて表示するように

--- src/components/FirstView.jsx
+++ src/components/FirstView.jsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
 import { MenuList } from "./index";
 import { catchCopy, siteTitle } from "../assets/js/contentList";
+
+import Logo from '../assets/img/logo_tate.svg';
+
 import '../assets/scss/_fitstview.scss';
 
 const FirstView = () => {
+    const [loaded, setLoad] = useState(false);
+
+    useEffect(() => {
+        setLoad(true);
+    }, [] )
+
     return(
         <div id="firstView">
-            <h1 className="top__siteTitle">{siteTitle.kanji}</h1>
+            <h1 className="top__siteTitle">
+                <img src={Logo} alt={siteTitle.kanji} className={`top__logo ${loaded ? "isLoaded" : "" }` } />
+            </h1>
             <h2 className="top__catchCopy">{catchCopy}</h2>
             <MenuList type="top" />
         </div>
