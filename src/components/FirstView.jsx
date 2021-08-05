c1d4f6daab4da154634ea3acf7fd4b3d84d240e2
Shumepi <shumpei.o.7g@gmail.com>
2021-08-06 02:03:19 +0900

    Update firstview 背景画像を追加してだいたいの雰囲気をつかめるように

--- src/components/FirstView.jsx
+++ src/components/FirstView.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { MenuList } from "./index";
-import { catchCopy, siteTitle } from "../assets/js/contentList";
+import { siteTitle } from "../assets/js/contentList";
 
 import Logo from '../assets/img/logo_tate.svg';
 
@@ -15,11 +15,15 @@ const FirstView = () => {
 
     return(
         <div id="firstView">
+            <div className="top__bg__wrapper">
+                <div className="top__bg"></div>
+            </div>
             <h1 className="top__siteTitle">
                 <img src={Logo} alt={siteTitle.kanji} className={`top__logo ${loaded ? "isLoaded" : "" }` } />
             </h1>
-            <h2 className="top__catchCopy">{catchCopy}</h2>
+            <p className={`top__catchCopy ${loaded ? "isLoaded" : "" }` }>実際の廃校を舞台に<span>謎解き</span> × <span>お化け屋敷</span>を体験！</p>
             <MenuList type="top" />
+            <p className="top__appeal">日本初ホラーミステリーツアー</p>
         </div>
     )
 }
