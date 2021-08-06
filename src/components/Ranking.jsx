bd7549ae1a5d8e27a3c3023270edc1acb3565c3d
Shumepi <shumpei.o.7g@gmail.com>
2021-08-06 16:28:58 +0900

    Update ranking ざっとスタイリング完了

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -5,33 +5,37 @@ const Ranking = () => {
         <div id="ranking" className="contents">
             <h2>ランキング</h2>
             <div>
-                <p>期間中、クリアタイムのランキングを掲載します。デイリーランキングと総合ランキングを毎日発表！
-Twitterとも連動しているので、逐一チェックすることができます。</p>
+                <p>期間中、クリアタイムのランキングを掲載します。</p>
+                <p>デイリーランキングと総合ランキングを毎日発表！Twitterとも連動しているので、逐一チェックすることができます。</p>
             </div>
             <div className="ranking__container">
                 <div className="ranking__wrapper">
-                    <p>総合ランキング</p>
+                    <p className="ranking__label">総合ランキング</p>
                     <div>
                         <p>
-                        1位   モック山テスト太郎<br />
+                        1位　モック山テスト太郎<br />
                         2位　アール川スペックの介<br />
                         3位　難読漢字01-_;:/+&gth;script<br />
                         </p>
                     </div>
                 </div>
                 <div className="ranking__wrapper">
-                    <p>デイリーランキング</p>
+                    <p className="ranking__label">デイリーランキング</p>
                     <div>
                         <p>
-                        1位   モック山テスト太郎<br />
+                        1位　モック山テスト太郎<br />
                         2位　アール川スペックの介<br />
                         3位　難読漢字01-_;:/+&gth;script<br />
                         </p>
                     </div>
                 </div>
             </div>
-            <div>
+            <div className="ranking__other_ranks">
                 <p>10位までのランキングを見る ▼</p>
+            </div>
+            <div className="ranking__sns_intro">
+                <p>イベント公式Twitterをフォロー！</p>
+                <a href="" target="_blank" rel="nofollow noopener">Twitter</a>
             </div>
         </div>
     )
